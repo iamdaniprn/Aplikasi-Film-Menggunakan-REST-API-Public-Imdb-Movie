0863abf9551b28c09b0b6f751236bb16447fdc3f
iamdaniprn <dani46purnama@gmail.com>
2019-02-04 12:03:12 +0700

    REST API Pertama

---
+++ json/latihan1.php
@@ -0,0 +1,19 @@
+<?php 
+
+$mahasiswa = [
+	[
+		"nama" => "Dani Purnama",
+		"nim" => "1147050034",
+		"email" => "danipurnama@gmail.com"
+	],
+	[
+		"nama" => "Dani Purnama",
+		"nim" => "1147050034",
+		"email" => "danipurnama@gmail.com"
+	]
+];
+
+$data = json_encode($mahasiswa);
+echo $data;
+
+ ?>
